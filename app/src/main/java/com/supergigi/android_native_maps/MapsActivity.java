6bbe110f75a2dd0822340a323b7f040809717d85
tedwei <tedwei@truetel.com>
2016-12-28 09:19:43 +0800

    Indoor Maps

--- app/src/main/java/com/supergigi/android_native_maps/MapsActivity.java
+++ app/src/main/java/com/supergigi/android_native_maps/MapsActivity.java
@@ -38,13 +38,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
-        // Other supported types include: MAP_TYPE_NORMAL,
-        // MAP_TYPE_SATELLITE, MAP_TYPE_TERRAIN, MAP_TYPE_HYBRID and MAP_TYPE_NONE
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        // Some buildings have indoor maps. Center the camera over
+        // the building, and a floor picker will automatically appear.
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(-33.86997, 151.2089), 18));
     }
 }
