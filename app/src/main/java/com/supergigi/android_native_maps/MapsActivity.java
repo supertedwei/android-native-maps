34e76d5ffa3b2c77f37fb93a5eeff26fa4af55aa
tedwei <tedwei@truetel.com>
2016-12-28 09:35:00 +0800

    Polylines

--- app/src/main/java/com/supergigi/android_native_maps/MapsActivity.java
+++ app/src/main/java/com/supergigi/android_native_maps/MapsActivity.java
@@ -12,6 +12,7 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -38,26 +39,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap map) {
 
-        LatLng mapCenter = new LatLng(41.889, -87.622);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(-18.142, 178.431), 2));
 
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(mapCenter, 13));
-
-        // Flat markers will rotate when the map is rotated,
-        // and change perspective when the map is tilted.
-        map.addMarker(new MarkerOptions()
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_room_white_24dp))
-                .position(mapCenter)
-                .flat(true)
-                .rotation(245));
-
-        CameraPosition cameraPosition = CameraPosition.builder()
-                .target(mapCenter)
-                .zoom(13)
-                .bearing(90)
-                .build();
-
-        // Animate the change in camera view over 2 seconds
-        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),
-                2000, null);
+        // Polylines are useful for marking paths and routes on the map.
+        map.addPolyline(new PolylineOptions().geodesic(true)
+                .add(new LatLng(-33.866, 151.195))  // Sydney
+                .add(new LatLng(-18.142, 178.431))  // Fiji
+                .add(new LatLng(21.291, -157.821))  // Hawaii
+                .add(new LatLng(37.423, -122.091))  // Mountain View
+        );
     }
 }
